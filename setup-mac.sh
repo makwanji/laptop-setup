1890e6b5fdc0ce5274756163e2879682d238217c
makwaji <makwana.jignesh@gmail.com>
2022-03-01 21:45:05 +0800

    Format code

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,5 +1,6 @@
 echo "###### Mac setup start"
 
+echo ">>---> Setup and cofnigure zsh"
 # Install starship
 brew install starship
 brew install zsh-autosuggestions
@@ -9,13 +10,18 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 #configure zsh
 cp ./mac/zshrc ~/.zshrc
-echo "###### setup end"
 
+
+echo ">>---> Install meetingbar"
 # Install meetingbar - https://github.com/leits/MeetingBar
 brew install meetingbar
 
+echo ">>---> Install stats"
 # Install stats - https://github.com/exelban/stats
 brew install --cask stats
 
+echo ">>---> Install k9s"
 # Install k9s
 brew install k9s
+
+echo "###### setup end"
