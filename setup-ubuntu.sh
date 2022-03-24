e08eae625a9e8a0c4feb96289ced2c3b72dc6498
makwanji <makwana.jignesh@gmail.com>
2022-03-24 17:19:19 +0800

    Added path in Ubuntu script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,11 +1,9 @@
 echo "###### Ubuntu Laptop setup start"
 
-
 #Install security update
-sh ubuntu/install-security-update.sh
+sh ./ubuntu/install-security-update.sh
 
 # Install VSCode
-sh ubuntu/install-vscode.sh
-
+sh ./ubuntu/install-vscode.sh
 
 echo "###### Ubuntu Laptop setup end"
