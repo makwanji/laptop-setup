e27b76465084c1a9afbb49cd7447dcc577394da3
makwanji <makwana.jignesh@gmail.com>
2022-03-24 17:20:03 +0800

    change message

--- ubuntu/install-vscode.sh
+++ ubuntu/install-vscode.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-echo "Install VSCode"
+echo "Installing VSCode"
 sudo apt update
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
