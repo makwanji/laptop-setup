4ba006d763086c68f10ba6938210f6673afc20a4
Jignesh Makwana <jignesh.makwana@adnovum.sg>
2023-09-03 11:29:14 +0800

    flutter installation

--- ubuntu/install-flutter-dev-tools.sh
+++ ubuntu/install-flutter-dev-tools.sh
@@ -6,8 +6,11 @@ sudo snap install postman
 # Ref - https://docs.flutter.dev/get-started/install/linux
 echo "Installing flutter........................"
 sudo snap install flutter --classic
-# sudo snap install android-studio --classic
+sudo snap install android-studio --classic
 
-# Install KVM
-# Ref : https://developer.android.com/studio/run/emulator-acceleration?utm_source=android-studio#vm-linux
-sudo apt-get install -Y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+flutter --version
+flutter doctor
+
+# # Install KVM
+# # Ref : https://developer.android.com/studio/run/emulator-acceleration?utm_source=android-studio#vm-linux
+# sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
