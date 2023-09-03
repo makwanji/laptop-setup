87c5cd4201756d474885c23bc4d9cf0075c8f161
Jignesh Makwana <jignesh.makwana@adnovum.sg>
2023-09-03 11:03:42 +0800

    SSH Keys copys

--- ubuntu/install-basic-tools.sh
+++ ubuntu/install-basic-tools.sh
@@ -4,3 +4,7 @@ echo "Install screen"
 sudo apt update
 sudo apt install -y snapd screen htop cpu-checker openssh-server
 sudo ufw allow ssh
+
+# Copy keys
+touch .ssh/authorized_keys
+cat ../kyes/jig.pub >> .ssh/authorized_keys
